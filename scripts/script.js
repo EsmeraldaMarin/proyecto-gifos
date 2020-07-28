22ac7ff37369e91f7bf2306b787288939e0c80d2
Esmeralda Marin <esmemarinm03@gmail.com>
2020-07-28 01:12:10 -0300

    seccion crear gifos terminado

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,6 @@
 const apiKeyGiphy = "v6GX2EfRqxwiexQZkHhYu6ZrteDkFt6Z";
+
 /*
-//cambiar color de fondos
-
-let bodyBg = document.querySelector("body");
-let header = document.querySelector("header");
-
-bodyBg.className = "bg_nocturno";
-header.className = "bg_nocturno";
-
-let trendingBg = document.getElementsByClassName("trending_gifos");
-trendingBg[0].classList.add("bg_nocturno_trendings");
-
-//cambiar color a imagenes e iconos
-
-let imgDiurno = document.getElementsByClassName("diurno");
-let imgNocturno = document.getElementsByClassName("nocturno");
-
-for (let i = 0; i < imgDiurno.length; i++) {
-    imgDiurno[i].style.display = "none";
-}
-for (let i = 0; i < imgNocturno.length; i++) {
-    imgNocturno[i].style.display = "block";
-}
-
-//cambiar color a las fuentes
-
-let textoNocturno = document.getElementsByClassName("txt_diurno");
-for (let i = 0; i < textoNocturno.length; i++) {
-    textoNocturno[i].classList.remove("txt_d");
-    textoNocturno[i].classList.add("txt_n");
-}
-
-//cambiar color inputs
-
-let inputNocturno = document.getElementsByTagName("input");
-for (let i = 0; i < inputNocturno.length; i++) {
-    inputNocturno[i].className = "input_n"
-}
-*/
-
 let body = document.querySelector("body");
 
-body.className = "nocturno"
+body.className = "body_nocturno";*/
