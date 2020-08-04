1746092121950edcab15af7c3a7c85d1abe9eb09
Esmeralda Marin <esmemarinm03@gmail.com>
2020-08-04 18:23:06 -0300

    modo nocturno retocado, detalles arreglados

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,19 @@
 const apiKeyGiphy = "v6GX2EfRqxwiexQZkHhYu6ZrteDkFt6Z";
 
-let modoNocturno = document.getElementById("modos");
+let body = document.querySelector("body");
+let buttonModoN = document.querySelector(".m_noc");
+let buttonModoD = document.querySelector(".m_di")
 
-modoNocturno.addEventListener("click", (e)=>{
-    e.preventDefault();
 
-    let body = document.querySelector("body");
+buttonModoN.addEventListener("click", modoNocturno);
+buttonModoD.addEventListener("click", modoNocturno);
+
+
+function modoNocturno(){
     body.classList.toggle("body_nocturno");
+    buttonModoD.classList.toggle("modo_noc");
+    buttonModoD.classList.toggle("m_di");
+    buttonModoN.classList.toggle("modo_noc");
+    buttonModoN.classList.toggle("m_noc");
 
-    modoNocturno.textContent="MODO DIURNO";
-})
-
+}
