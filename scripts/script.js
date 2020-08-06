527020018d16d83c581e36d3b4e99926da0dc92f
Esmeralda Marin <esmemarinm03@gmail.com>
2020-08-06 18:01:35 -0300

    modo nocturno en el local storage

--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,19 @@ const apiKeyGiphy = "v6GX2EfRqxwiexQZkHhYu6ZrteDkFt6Z";
 
 let body = document.querySelector("body");
 let buttonModoN = document.querySelector(".m_noc");
-let buttonModoD = document.querySelector(".m_di")
+let buttonModoD = document.querySelector(".m_di");
 
+body.className = localStorage.getItem("theme");
+
+function modoNocturno(){
+    body.classList.toggle("body_nocturno");
+    let bodyClass = body.className
+    saveTheme(bodyClass);
+}
+let saveTheme = (bodyClass)=>{
+    localStorage.setItem("theme", bodyClass)
+}
 
 buttonModoN.addEventListener("click", modoNocturno);
 buttonModoD.addEventListener("click", modoNocturno);
 
-
-function modoNocturno(){
-    body.classList.toggle("body_nocturno");
-    buttonModoD.classList.toggle("modo_noc");
-    buttonModoD.classList.toggle("m_di");
-    buttonModoN.classList.toggle("modo_noc");
-    buttonModoN.classList.toggle("m_noc");
-
-}
